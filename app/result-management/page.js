524a84b790b6714290b429abc3ee3b1f0ff2d0da
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-05-10 17:06:27 +0530

    aded changes

--- app/result-management/page.js
+++ app/result-management/page.js
@@ -177,31 +177,14 @@ const page = () => {
 	};
 
 	useEffect(() => {
-		apiforResults(); // Initial call to apiforResults
+		apiforResults();
 
-		const roundedTimeInMilliseconds = getRoundedISOTime(); // Get the next rounded time in milliseconds
-		const currentTimeInMilliseconds = moment().valueOf(); // Get current time in milliseconds
-
-		// Calculate the delay before the first execution
-		const delay = roundedTimeInMilliseconds - currentTimeInMilliseconds;
-
-		// Set the interval based on the rounded time, with an additional delay before the first execution
 		const interval = setInterval(() => {
 			apiforResults();
 			autoSubmitResult();
-		}, 15 * 60 * 1000); // Set interval to 15 minutes (in milliseconds)
+		}, 1000);
 
-		// Delay the first execution based on the rounded time
-		if (delay > 0) {
-			setTimeout(() => {
-				apiforResults();
-				autoSubmitResult();
-			}, delay); // Delay the first execution
-		}
-
-		return () => {
-			clearInterval(interval); // Clear the interval when the component unmounts
-		};
+		return () => clearInterval(interval);
 	}, []);
 
 	return (
